b28a29539e0d4489a727e70b6df91573bfa873cf
yaramamd0uh <162519947+yaramamd0uh@users.noreply.github.com>
2025-08-15 03:31:22 +0300

    Add files via upload

---
+++ ITIday2.py
@@ -0,0 +1,70 @@
+'''
+class myqueue():
+    def __init__(self):
+        self.que=[]
+        self.length=0
+    def enqueue(self,item):
+        self.que.append(item)
+        self.length+=1
+
+    def dequeue(self):
+        print("dequeue",self.que[0])
+        for i in range(0,self.length-1): 
+            self.que[i]=self.que[i+1]
+        self.length-=1
+
+    def isempty(self):
+        if self.length==0:
+           print( "empty")
+
+    def display(self): 
+        for i in range(0,self.length):
+            print(self.que[i])
+
+
+que=myqueue()
+que.isempty()
+que.enqueue(1)
+que.enqueue(2)
+que.enqueue(3)
+que.display()
+que.dequeue()
+que.display()
+
+'''
+
+
+
+class Car:
+    def __init__(self, make, model, fuel_level):
+        self.make = make
+        self.model = model
+        self._fuel_level = fuel_level
+
+    def refuel(self, amount):
+        self._fuel_level += amount
+        return self._fuel_level
+
+    def get_fuel_level(self):
+        print(f"Current level: {self._fuel_level}")
+
+
+class electric_car(Car):
+    def __init__(self,  battery_level):
+        super().__init__(make, model, fuel_level)
+        self.__battery_level = battery_level
+
+    def charge(self, amount):
+        self.__battery_level += amount
+        return self.__battery_level
+
+    @classmethod
+    def from_string(cls, car_str, fuel_level, battery_level):
+        make, model = car_str.split("-")
+        return cls(make, model, fuel_level, battery_level)
+
+e1 = electric_car .from_string("Tesla-Model3", 10, 80)
+e1.get_fuel_level()     
+print(e1.charge(20))
+e1.refuel(15)
+e1.get_fuel_level()
